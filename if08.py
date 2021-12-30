1a7f44ffb310d7cfa6013ee95412747e5ef14512
Murodjon <murodjondeveloper@gmail.com>
2021-12-30 23:27:08 +0500

    if08

--- if08.py
+++ if08.py
@@ -11,4 +11,13 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a//10>1 and a//10<=9 and a%2==0:
+        return "two-digit odd number"
+    elif a//10>1 and a//10<=9 and a%2==1:
+        return "two-digit even number"
+    elif a//100>1 and a//100<=9 and a%2==0:
+        return "three-digit odd number"
+    elif a//100>1 and a//100<=9 and a%2==1:
+        return "three-digit even number"
+    
+    
